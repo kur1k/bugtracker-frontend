a32bd56674799c148b7d7391e3b274404f5e6349
igor <igorson2002@gmail.com>
2026-06-03 23:17:14 +0300

    fixes

--- src/api.js
+++ src/api.js
@@ -1,24 +1,40 @@
 import axios from "axios";
 
-const API = axios.create({
+// создаём единый axios-инстанс
+export const API = axios.create({
     baseURL: "https://bugtracker-backend-2.onrender.com/api",
 });
 
-// Автоматически добавляет токен к каждому запросу
+// автоматически добавляем JWT токен ко ВСЕМ запросам
 API.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
+
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
+    } else {
+        delete config.headers.Authorization;
     }
+
     return config;
 });
 
+// -------------------- AUTH --------------------
 export const login = (username, password) =>
     API.post("/login/", { username, password });
 
-export const getBugs = () => API.get("/bugs/");
-export const createBug = (data) => API.post("/bugs/", data);
+// -------------------- BUGS --------------------
+export const getBugs = () =>
+    API.get("/bugs/");
 
-export const getUsers = () => API.get("/users/");
-export const updateUser = (id, data) => API.put(`/users/${id}/`, data);
-export const deleteUser = (id) => API.delete(`/users/${id}/`);
+export const createBug = (data) =>
+    API.post("/bugs/", data);
+
+// -------------------- USERS --------------------
+export const getUsers = () =>
+    API.get("/users/");
+
+export const updateUser = (id, data) =>
+    API.put(`/users/${id}/`, data);
+
+export const deleteUser = (id) =>
+    API.delete(`/users/${id}/`);
